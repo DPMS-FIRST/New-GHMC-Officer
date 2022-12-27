8274277bdf24c133f18a3b9bb0b481a6f1415fd6
DPMS-FIRST <87933368+DPMS-FIRST@users.noreply.github.com>
2022-12-27 15:00:26 +0530

    abstract report

--- lib/View/googlemaps.dart
+++ lib/View/googlemaps.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:ghmc_officer/Res/constants/routes/app_routes.dart';
+import 'package:ghmc_officer/Res/constants/text_constants/text_constants.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapSample extends StatefulWidget {
@@ -24,28 +26,100 @@ class MapSampleState extends State<MapSample> {
       target: LatLng(17.436617, 78.3608504),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+
+  @override
+  void initState() {
+    addCustomIcon();
+    super.initState();
+  }
+
+  void addCustomIcon() {
+    //showAlert();
+    BitmapDescriptor.fromAssetImage(
+            const ImageConfiguration(), "assets/Location_marker.png")
+        .then(
+      (icon) {
+        setState(() {
+          markerIcon = icon;
+        });
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     appBar: AppBar(
+      actions: [
+            IconButton(
+              icon: Icon(Icons.home, color: Colors.black),
+              onPressed: () async{
+                //EasyLoading.show();
+                Navigator.pushNamed(context, AppRoutes.ghmcdashboard);
+              },
+            ),
+          ],
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: Text(
+          TextConstants.full_grievance_details,
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        
+      ),
       body: GoogleMap(
         mapType: MapType.normal,
         //markers: Set<Marker>.of(_markers),
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
+        }, 
+        markers: {
+          Marker(
+            markerId: const MarkerId("marker1"),
+            position: const LatLng(17.436617, 78.3608504),
+            draggable: true,
+            onDragEnd: (value) {
+              // value is the new position
+            },
+            icon: markerIcon,
+          ),
         },
       ),
-      floatingActionButton: FloatingActionButton.extended(
+      /* floatingActionButton: FloatingActionButton.extended(
         onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
+         label: const Text('To'),
         icon: const Icon(Icons.directions_boat),
       ),
+        */
+        bottomSheet: Container(
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height * 0.05,
+                      margin: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.3),
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                         Navigator.pop(context);
+                        },
+                        child: Text(
+                          "Set Location",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      )),
     );
   }
 
-  Future<void> _goToTheLake() async {
+   Future<void> _goToTheLake() async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
+  } 
+  
+
 }
